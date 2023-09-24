a575a88d85508f2e2ccffc099166a3c6784b7bb0
opoku1968 <>
2023-09-24 00:34:38 +0000

    Function to run when game over

--- assets/js/script.js
+++ assets/js/script.js
@@ -86,6 +86,42 @@ const choiceNumber = Math.floor(Math.random() * 3);
             }
         }
     }
+    // Function to run when game is over
+    const gameOver = (playerOptions, movesLeft) => {
+
+        const chooseMove = document.querySelector('.move');
+        const result = document.querySelector('.result');
+        const reloadBtn = document.querySelector('.reload');
+
+        playerOptions.forEach(option => {
+            option.style.display = 'none';
+        })
+
+
+        chooseMove.innerText = 'Game Over!!';
+        movesLeft.style.display = 'none';
+
+        if (playerScore > computerScore) {
+            result.style.fontSize = '2rem';
+            result.innerText = 'You Won The Game';
+            result.style.color = '#308D46';
+        }
+        else if (playerScore < computerScore) {
+            result.style.fontSize = '2rem';
+            result.innerText = 'You Lost The Game';
+            result.style.color = 'red';
+        }
+        else {
+            result.style.fontSize = '2rem';
+            result.innerText = 'Tie';
+            result.style.color = 'grey';
+        }
+        reloadBtn.innerText = 'Restart';
+        reloadBtn.style.display = 'flex';
+        reloadBtn.addEventListener('click', () => {
+            window.location.reload();
+        })
+    }
 
 
                 
