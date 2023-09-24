45216640547959bc235956c4f48c6d263f6aa66f
opoku1968 <>
2023-09-24 00:06:36 +0000

    creat const logic of game

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+// Complete logic of game inside this function
+const game = () => {
+    let playerScore = 0;
+    let computerScore = 0;
+    let moves = 0;
+
+
+// Function to
+
+
+
+
+// Function to start playing game
+
+
+ // Function to check who wins
+
+ 
+ // Calling gameOver function after 10 moves
+                
