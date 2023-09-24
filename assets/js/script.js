41727179e17ad800fb61d603a0aacad047be6301
opoku1968 <>
2023-09-24 00:14:43 +0000

    function to check who wins

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,31 +6,31 @@ const game = () => {
 
 
 // Function to
-    const playGame = () => {
-        const rockBtn = document.querySelector('.rock');
-        const paperBtn = document.querySelector('.paper');
-        const scissorBtn = document.querySelector('.scissor');
-        const playerOptions = [rockBtn, paperBtn, scissorBtn];
-        const computerOptions = ['rock', 'paper', 'scissors']
+const playGame = () => {
+const rockBtn = document.querySelector('.rock');
+const paperBtn = document.querySelector('.paper');
+ const scissorBtn = document.querySelector('.scissor');
+ const playerOptions = [rockBtn, paperBtn, scissorBtn];
+ const computerOptions = ['rock', 'paper', 'scissors']
 
 
 
 // Function to start playing game
-        playerOptions.forEach(option => {
-            option.addEventListener('click', function () {
+ playerOptions.forEach(option => {
+ option.addEventListener('click', function () {
 
-                const movesLeft = document.querySelector('.movesleft');
-                moves++;
-                movesLeft.innerText = `Moves Left: ${10 - moves}`;
+ const movesLeft = document.querySelector('.movesleft');
+ moves++;
+ movesLeft.innerText = `Moves Left: ${10 - moves}`;
 
 
-                const choiceNumber = Math.floor(Math.random() * 3);
-                const computerChoice = computerOptions[choiceNumber];
+const choiceNumber = Math.floor(Math.random() * 3);
+ const computerChoice = computerOptions[choiceNumber];
 
 
 
  // Function to check who wins
-
+ winner(this.innerText, computerChoice)
  
  // Calling gameOver function after 10 moves
                 
